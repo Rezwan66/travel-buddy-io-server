e3c9cefb1731be84a3ce24a6211dff8b3b2c7a3e
Rezwan66 <shaikhrezwan66@gmail.com>
2023-11-06 00:42:49 +0600

    implemented search functionality by using query in the req

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(cookieParser());
 
 // MONGO DB
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zjzxbzp.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,11 +30,34 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
-        const serviceCollection = client.db('travelBuddyDB').collection('services')
-        const bookingCollection = client.db('travelBuddyDB').collection('bookings')
+        const serviceCollection = client.db('travelBuddyDB').collection('services');
+        const bookingCollection = client.db('travelBuddyDB').collection('bookings');
+
+        // SERVICES related API
+        app.get('/services', async (req, res) => {
+            try {
+                let queryObj = {};
+                const serviceName = req.query.serviceName;
+                console.log(serviceName);
+
+                if (serviceName) {
+                    // set a searchPattern with $regex to find case-insensitive service name.
+                    const searchPattern = new RegExp(serviceName, 'i');
+                    queryObj.service_name = { $regex: searchPattern }
+                }
+
+                // db.InspirationalWomen.find({ first_name: { $regex: /serviceName/i } })
+
+                const cursor = serviceCollection.find(queryObj);
+                const result = await cursor.toArray();
+                res.send(result);
+            } catch (error) {
+                console.log(error);
+            }
+        })
 
     } finally {
         // Ensures that the client will close when you finish/error
